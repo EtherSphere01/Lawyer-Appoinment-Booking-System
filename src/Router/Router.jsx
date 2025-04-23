1238d8c6735b2170b81b4d5ba331dcca236ac251
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-23 18:52:06 +0600

    router added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,7 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "../Root/Root";
+import Error from "../Pages/Error/Error";
+import Header from "../components/Header/Header";
+import Home from "../Pages/Home/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
 ]);
